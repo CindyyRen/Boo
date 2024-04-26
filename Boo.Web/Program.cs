6bf5dada2586279ceefc29386b68bad7409c3fb2
Cindy Ren <bdcindy78@gmail.com>
2024-04-26 20:49:06 +1000

    databse and villa table setup

--- Boo.Web/Program.cs
+++ Boo.Web/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Boo.Infrastructure.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+Console.WriteLine($"Connection string: {connectionString}");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
