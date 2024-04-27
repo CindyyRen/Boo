9d570fb04ac3263d0e3699913976cd742e8ab673
Cindy Ren <bdcindy78@gmail.com>
2024-04-27 13:25:01 +1000

    delete-villa

--- Boo.Web/Controllers/VillaController.cs
+++ Boo.Web/Controllers/VillaController.cs
@@ -60,5 +60,26 @@ namespace Boo.Web.Controllers
             }
             return View();
         }
+        public IActionResult Delete(int villaId)
+        {
+            Villa? obj = _db.Villas.FirstOrDefault(u => u.Id == villaId);
+            if (obj is null)
+            {
+                return RedirectToAction("Error", "Home");
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        public IActionResult Delete(Villa obj)
+        {
+            Villa? objFromDb = _db.Villas.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb is not null)
+            {
+                _db.Villas.Remove(objFromDb);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
